a30858da11177775fe0fad030271b9b904d9e4ac
Hayeu Yury <hayeuyur@fit.cvut.cz>
2021-07-07 20:07:15 +0200

    Remove files

--- ideas/AST-Interface/Ideas/GenericASTInterface/Nodes/Call.py
+++
@@ -1,6 +0,0 @@
-from ..Base.Node import Node
-
-class Call(Node):
-
-  def __init__(self, parent, id: str, value: str, children) -> None:
-      super().__init__(parent, id, 'call', value, children)
