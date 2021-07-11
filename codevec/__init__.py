f44d2858559b10a1da627ecb905395553db89c20
Hayeu Yury <hayeuyur@fit.cvut.cz>
2021-07-11 20:39:38 +0200

    Add imports in init.py

---
+++ codevec/__init__.py
@@ -0,0 +1,5 @@
+from .utils import Features
+
+from .models import BSSM
+from .models import MaxPooling
+from .models import Transformer
---
+++ codevec/models/__init__.py
@@ -0,0 +1,3 @@
+from .BSSM import BSSM
+from .MaxPooling import MaxPooling
+from .Transformer import Transformer
