7796fe9dd326ee8f0e2f4698004845b15a72fbdd
Hayeu Yury <hayeuyur@fit.cvut.cz>
2021-07-11 20:24:52 +0200

    Update main.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
   name = 'codevec',
   version = '0.0.1',
   license = 'MIT',
-  package_dir = { "": "codevec" },
-  packages = "codevec"
+  package_dir = { "": "." },
+  packages = ["codevec"]
 )
